dd3e16c5efbd110bcccca1b0e8333244535024b7
izilongwan <js.nb@qq.com>
2026-01-20 14:07:37 +0800

    feat: 更新WebCommonAutoConfiguration类，添加AmqpConfig导入以支持RabbitMQ配置

--- java_21_demo-web-common/src/main/java/com/java_21_demo/web/config/WebCommonAutoConfiguration.java
+++ java_21_demo-web-common/src/main/java/com/java_21_demo/web/config/WebCommonAutoConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 import com.github.xiaoymin.knife4j.spring.configuration.Knife4jProperties;
 import com.java_21_demo.web.advice.MicoAppCommonWebResponseAdvice;
 import com.java_21_demo.web.aspect.MicoAppCommonWebResponseAspect;
+import com.java_21_demo.web.config.property.AmqpConfig;
 import com.java_21_demo.web.config.property.ExceptionProperty;
 import com.java_21_demo.web.config.property.WebResultBodyProperty;
 import com.java_21_demo.web.filter.MicoAppCommonClearFilter;
@@ -16,21 +17,22 @@ import com.java_21_demo.web.filter.MicoAppCommonClearFilter;
  * 统一管理所有需要自动装配的配置类
  */
 @Configuration
-@EnableConfigurationProperties({ WebResultBodyProperty.class, Knife4jProperties.class, ExceptionProperty.class })
+@EnableConfigurationProperties({WebResultBodyProperty.class, Knife4jProperties.class, ExceptionProperty.class})
 @Import({
-                MicoAppCommonWebResponseAdvice.class,
-                MicoAppCommonWebResponseAspect.class,
-                MicoAppCommonJacksonConverterConfig.class,
-                MicoAppCommonWebExceptionConfig.class,
-                MicoAppCommonValidatorConfig.class,
-                MicoAppCommonWebMvcConfig.class,
-                MicoAppCommonClearFilter.class,
-                Knife4jConfig.class
+    MicoAppCommonWebResponseAdvice.class,
+    MicoAppCommonWebResponseAspect.class,
+    MicoAppCommonJacksonConverterConfig.class,
+    MicoAppCommonWebExceptionConfig.class,
+    MicoAppCommonValidatorConfig.class,
+    MicoAppCommonWebMvcConfig.class,
+    MicoAppCommonClearFilter.class,
+    Knife4jConfig.class,
+    AmqpConfig.class
 })
 public class WebCommonAutoConfiguration {
 
-        /**
-         * 集中的自动装配配置入口
-         * 所有 web-common 相关的配置都在此聚合
-         */
+    /**
+     * 集中的自动装配配置入口
+     * 所有 web-common 相关的配置都在此聚合
+     */
 }
